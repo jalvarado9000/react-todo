6e1bc035357bb61df26311349bcecaf6d255945e
jalvarado9000 <jalvarado9000@gmail.com>
2024-02-21 22:58:51 -0800

    finished

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -25,6 +25,8 @@ export const AddTodoForm = (props) => {
 
   
 
+  
+
  
 
 
@@ -32,12 +34,12 @@ export const AddTodoForm = (props) => {
 
   return (
     <div className="text-bg-dark container-lg">
-        <form action="" className="m-5 pb-1" onSubmit={handleAddTodo}>
+        <form className="m-5 pb-1" onSubmit={handleAddTodo}>
           <label htmlFor={todoTitle} className="h5 m-1 mt-3">
             Title
           </label>
           <input name="title" type="text" id={todoTitle} />
-          <input type="submit" className="btn btn-primary m-1 mb-2" value="Add"  />
+          <input type="submit" className="btn btn-primary m-1 mb-2" value="Add" />
         </form>
 
        
