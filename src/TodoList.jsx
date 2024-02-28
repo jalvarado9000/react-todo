0e5aace34526328a44eafb9e578942fd332bad0f
jalvarado9000 <jalvarado9000@gmail.com>
2024-02-27 22:49:46 -0800

    updated some things

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,38 +2,39 @@ import React from 'react'
 
 
 const todoList = [
-    {
-      id: 1,
-      title: 'Complete Assigment',
-  
-    },
-    {
-      id: 2,
-      title: 'Complete Homework',
-    },
-    {
-      id: 3,
-      title: 'Complete Projects',
-    },
-  
-  ]
-  
+  {
+    id: 1,
+    title: 'Complete Assigment',
+
+  },
+  {
+    id: 2,
+    title: 'Complete Homework',
+  },
+  {
+    id: 3,
+    title: 'Complete Projects',
+  },
+
+]
+
 const TodoList = () => {
   return (
     <div>
-    <h1>Todo List</h1>
+      <h1>Todo List</h1>
 
-    <ul className="list-group">
-        {todoList.map(function (item) {
-          return (
+      <ul className="list-group">
+        {todoList.map((item) => {
+          (
             <li className="list-group-item hover-overlay thumbnail fw-bolder" key={item.id}>{item.title}</li>
-            
-          
-            );
+
+
+          );
         })}
       </ul>
-      </div>
+    </div>
   )
 }
 
 export default TodoList
+
