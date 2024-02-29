7d5c33f501c5bed2c2baeb1bd74b040341c4e966
jalvarado9000 <jalvarado9000@gmail.com>
2024-02-28 17:37:53 -0800

    making some updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import './TodoList'
 import TodoList from './TodoList'
-import { AddTodoForm } from './AddTodoForm'
+import AddTodoForm  from './AddTodoForm'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
