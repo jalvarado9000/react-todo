e431cdd0c84437efa66c6b8bed03c0771ae8f62b
jalvarado9000 <jalvarado9000@gmail.com>
2024-03-09 23:24:20 -0800

    Now its working

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
+
 import './App.css'
+import './TodoList'
 import TodoList from './TodoList'
-import AddTodoForm  from './AddTodoForm'
+import { AddTodoForm } from './AddTodoForm'
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 function App() {
- 
+
 
   return (
     <>
       <TodoList />
       <AddTodoForm />
-      {/*on lesson 2 */}
-        
+      
+
     </>
-    );
-  }
+  );
+}
 
 export default App
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,40 +1,41 @@
 import React from 'react'
 
 
-const todoList = [
-  {
-    id: 1,
-    title: 'Complete Assigment',
 
-  },
-  {
-    id: 2,
-    title: 'Complete Homework',
-  },
-  {
-    id: 3,
-    title: 'Complete Projects',
-  },
-
-]
 
 const TodoList = () => {
+
+  const todoList = [
+    {
+      id: 1,
+      title: 'Complete Assigment',
+
+    },
+    {
+      id: 2,
+      title: 'Complete Homework',
+    },
+    {
+      id: 3,
+      title: 'Complete Projects',
+    },
+
+  ]
   return (
     <div>
       <h1>Todo List</h1>
 
       <ul className="list-group">
-        {todoList.map((item) => {
+        {todoList.map((item) => 
           (
             <li className="list-group-item hover-overlay thumbnail fw-bolder" key={item.id}>{item.title}</li>
 
 
-          );
-        })}
+          )
+        )}
       </ul>
     </div>
   )
 }
 
-export default TodoList
-
+export default TodoList;
