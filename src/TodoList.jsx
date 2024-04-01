7c4f2e5d9b110b8921b17a1e851de230224c6e71
jalvarado9000 <jalvarado9000@gmail.com>
2024-03-31 18:52:43 -0700

    Finished please review work.

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -9,7 +9,7 @@ const TodoList = ({ onTodo, onRemoveTodo }) => {
       <ul className="list-group">
         {onTodo.map((item) => {
           return (
-            <TodoListItem key={item.id} item={item} onRemoveTodo={onRemoveTodo}/>
+            <TodoListItem key={item.id} item={item} onRemoveTodo={onRemoveTodo} />
           );
         })}
       </ul>
