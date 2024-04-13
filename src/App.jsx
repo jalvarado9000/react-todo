8cb722b7ec5bd627581233879d14661ae5f9fca5
jalvarado9000 <jalvarado9000@gmail.com>
2024-04-13 02:21:27 -0700

    Completed GET & POST function, I am able to extract and submit information to the airtable database.

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,9 @@ function App() {
   const [todoList, setTodoList] = useState(initialState);
   const [isLoading, setIsLoading] = useState(true);
 
-
+  const AIRTABLE_API_TOKEN = import.meta.env.VITE_AIRTABLE_API_TOKEN;
+  const AIRTABLE_BASE_ID = import.meta.env.VITE_AIRTABLE_BASE_ID;
+  const TABLE_NAME = import.meta.env.VITE_TABLE_NAME;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -19,11 +21,11 @@ function App() {
       const options = {
         method: 'GET',
         headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_AIRTABLE_API_TOKEN}`
+          Authorization: `Bearer ${AIRTABLE_API_TOKEN}`
         }
       };
 
-      const url = `https://api.airtable.com/v0/${import.meta.env.VITE_AIRTABLE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+      const url = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`;
 
       try {
         const response = await fetch(url, options);
@@ -32,19 +34,20 @@ function App() {
           throw new Error(`Error: ${response.status}`);
 
         }
-
         const data = await response.json();
+
         console.log(data);
 
-        const t = data.records.map(record => ({
+        const todos = data.records.map(record => ({
           title: record.fields.title,
-          id: record.id
+          id: record.id,
+
         }));
 
+        console.log(todos[0].id);
 
-        setTodoList(t);
 
-
+        setTodoList(todos);
 
       } catch (error) {
         console.error(error.message);
@@ -56,7 +59,56 @@ function App() {
     };
 
     fetchData();
+
   }, []);
+
+
+
+
+
+  const postTodo = async (newTodo) => {
+    try {
+      const airtableData = {
+        records: [
+          {
+            fields: {
+              title: newTodo.title
+
+            }
+          }
+        ]
+      }
+
+
+      const response = await fetch(
+        `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/${TABLE_NAME}`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${AIRTABLE_API_TOKEN}`,
+          },
+          body: JSON.stringify(airtableData),
+        }
+      );
+
+
+
+      if (!response.ok) {
+        const message = `Error has ocurred:
+        ${response.status}`;
+        throw new Error(message);
+      }
+
+      const dataResponse = await response.json();
+      return dataResponse;
+    } catch (error) {
+      console.log(error.message);
+      return null;
+    }
+  };
+
+
 
   useEffect(() => {
     if (!isLoading) {
@@ -79,7 +131,7 @@ function App() {
       ) : (
         <>
           <TodoList onTodo={todoList} onRemoveTodo={removeTodo} />
-          <AddTodoForm onAddTodo={handleAddTodo} />
+          <AddTodoForm onAddTodo={handleAddTodo} postTodo={postTodo} />
         </>
       )}
     </>
